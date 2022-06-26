31a898b47049d95bd5e1ca3b7993545da2028202
Pedro Fernandes <ppedrohfs@gmail.com>
2022-06-26 15:12:09 -0300

    Adicionando o pacote CommunityToolkit.MVVM para geração automática do Binding das propriedades na ViewModel

--- CrazyMonkeys/ViewModel/BaseViewModel.cs
+++ CrazyMonkeys/ViewModel/BaseViewModel.cs
@@ -1,49 +1,58 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CrazyMonkeys.ViewModel
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public partial class BaseViewModel : ObservableObject
     {
-        private bool isBusy;
+        #region ImplementacaoSemSorceCodeGeneratorMVVM_Binding
+        //private bool isBusy;
+        //public bool IsBusy
+        //{
+        //    get => isBusy; //get { return isBusy; }
+        //    set
+        //    {
+        //        if (isBusy == value)
+        //            return;
 
-        public bool IsBusy
-        {
-            get => isBusy; //get { return isBusy; }
-            set
-            {
-                if (isBusy == value)
-                    return;
+        //        isBusy = value;
+        //        OnPropertyChange(nameof(isBusy));
+        //        OnPropertyChange(nameof(IsNotBusy));
+        //    }
+        //}
 
-                isBusy = value;
-                OnPropertyChange(nameof(isBusy));
-                OnPropertyChange(nameof(IsNotBusy));
-            }
-        }
+        //private string title;
 
-        private string title;
+        //public string Title
+        //{
+        //    get => title;
+        //    set
+        //    {
+        //        if (title == value)
+        //            return;
 
-        public string Title
-        {
-            get => title;
-            set
-            {
-                if (title == value)
-                    return;
+        //        title = value;
+        //        OnPropertyChange(nameof(title));
+        //    }
+        //}
 
-                title = value;
-                OnPropertyChange(nameof(title));
-            }
-        }
+        //public bool IsNotBusy => !IsBusy;
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        ////ppublic void OnPropertyChange([CallerMemberName]string name = null) => Compilador identifica a property que está chamando o método
+        //public void OnPropertyChange(string name)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        //}
+        #endregion
+
+        [ObservableProperty]
+        public string title;
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(IsNotBusy))]
+        public bool isBusy;
 
         public bool IsNotBusy => !IsBusy;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        //ppublic void OnPropertyChange([CallerMemberName]string name = null) => Compilador identifica a property que está chamando o método
-        public void OnPropertyChange(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
     }
 }
