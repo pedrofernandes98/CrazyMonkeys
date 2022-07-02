2914d29563f183e26e6d780fcc1e5eab08461638
Pedro Fernandes <ppedrohfs@gmail.com>
2022-07-02 17:02:09 -0300

    Adicionando a funcionalidade de mostrar a localização no Maps

--- CrazyMonkeys/ViewModel/MonkeyDetailsViewModel.cs
+++ CrazyMonkeys/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,17 +1,41 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CrazyMonkeys.Models;
+using Microsoft.Maui.ApplicationModel;
 
 namespace CrazyMonkeys.ViewModel
 {
     [QueryProperty(nameof(Monkey), "Monkey")]
     public partial class MonkeyDetailsViewModel : BaseViewModel
     {
-        public MonkeyDetailsViewModel()
+        IMap map;
+        public MonkeyDetailsViewModel(IMap map)
         {
-
+            this.map = map;
         }
 
         [ObservableProperty]
         Monkey monkey;
+
+        [ICommand]
+        async Task ShowLocationOnMapAsync()
+        {
+            try
+            {
+                await map.OpenAsync(Monkey.Latitude, Monkey.Longitude,
+                    new MapLaunchOptions
+                    {
+                        Name = Monkey.Name,
+                        NavigationMode = NavigationMode.None
+                    });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await Shell.Current.DisplayAlert("Erro!", 
+                    string.Format("Não foi possível exibir o Mapa: {0}", ex.Message), "Ok");
+
+            }
+        }
     }
 }
